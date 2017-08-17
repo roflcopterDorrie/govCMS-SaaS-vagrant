f7ec9e9ae809adc4ef0e61675ba3c57aaa0f39de
Josh Li <josli@deloitte.com.au>
2017-08-17 11:24:15 +1000

    Added gitignore.

--- docroot/sites/default/themes/site/uikit_base/STARTERKIT/template.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * @file
- * Contains the theme's functions to manipulate Drupal's default markup.
- *
- * Complete documentation for this file is available online.
- * @see https://drupal.org/node/1728096
- */
--- vendor/beet/box/provisioning/ansible/roles/beetbox-docker/files/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Start supervisor
-supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
